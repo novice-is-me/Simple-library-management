0a3b110b8c811a99a4e33205399443d4b7707cdd
Regina Jalandoni <reginajalandoni13@gmail.com>
2024-11-26 13:59:16 +0800

    Adding Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
+import Navbar from './components/Navbar.vue'
   console.log('setup')
 </script>
 
 <template>
   <header class="">
-    <h1 class=" text-3xl">Vue Library System </h1>
+    <Navbar />
   </header>
 
 </template>
